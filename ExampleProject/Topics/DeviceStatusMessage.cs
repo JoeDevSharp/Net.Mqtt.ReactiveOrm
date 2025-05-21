a9fda72e16e894f72f176d3dc7b5cf1832d7c712
José Miguel <clasiccode@gmail.com>
2025-05-21 19:58:32 +0200

    Actualiza el estado del mensaje del dispositivo a "Online"
    Se ha añadido una línea en la creación del objeto `DeviceStatusMessage` para establecer su estado como "Online". Esta modificación se realiza justo antes de la suscripción a los mensajes de estado del dispositivo.
    
    Cambio de espacio de nombres y propiedades añadidas
    
    Se ha cambiado el espacio de nombres de `ExampleProject.Models` a `ExampleProject.Topics`.
    Se han añadido las propiedades `Name`, `Temperature` y `Humidity` a la clase `DeviceStatusMessage`, con valores predeterminados de `string.Empty`, `0` y `0`, respectivamente.

--- ExampleProject/Topics/DeviceStatusMessage.cs
+++ ExampleProject/Topics/DeviceStatusMessage.cs
@@ -3,11 +3,14 @@ using Mqtt.net.ORM;
 using Mqtt.net.ORM.Attributes;
 using MQTTnet.Protocol;
 
-namespace ExampleProject.Models
+namespace ExampleProject.Topics
 {
 
     [MqttTopic("devices/{DeviceId}/status", MqttQualityOfServiceLevel.ExactlyOnce)]
     public class DeviceStatusMessage : TopicStatus
     {
+        public string Name { get; set; } = string.Empty;
+        public int Temperature { get; set; } = 0;
+        public int Humidity { get; set; } = 0;
     }
 }
