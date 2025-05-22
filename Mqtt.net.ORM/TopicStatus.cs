10593dab476cc4fb6427d085a8f6007b84934953
José Miguel <clasiccode@gmail.com>
2025-05-22 08:13:29 +0200

    Update TopicStatus.cs

--- Mqtt.net.ORM/TopicStatus.cs
+++ Mqtt.net.ORM/TopicStatus.cs
@@ -2,7 +2,7 @@
 {
     public class TopicStatus
     {
-        public string DeviceId { get; set; } = new Guid().ToString();
+        public Guid DeviceId { get; set; } = Guid.NewGuid();
         public string Status { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
