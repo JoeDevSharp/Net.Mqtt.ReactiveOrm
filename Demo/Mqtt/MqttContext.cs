451c6dc8d1dfbbadf9b50215f8a424e670c33eee
José Miguel <clasiccode@gmail.com>
2025-05-24 23:02:19 +0200

    Cambia referencia de paquete a proyecto local
    Actualiza `Demo.csproj` para usar una referencia de proyecto en lugar de un paquete NuGet para `Codevia.MqttReactiveObjectMapper`.
    
    Añade la importación de `Codevia.MqttReactiveObjectMapper.Enums` en `MqttContext.cs` y modifica el atributo `Topic` de `DHT230222_Modules` para incluir `QoSLevel.AtMostOnce` y el parámetro de retención.

--- Demo/Mqtt/MqttContext.cs
+++ Demo/Mqtt/MqttContext.cs
@@ -1,13 +1,14 @@
 ﻿using Demo.Entities;
 using Codevia.MqttReactiveObjectMapper;
 using Codevia.MqttReactiveObjectMapper.Attributes;
+using Codevia.MqttReactiveObjectMapper.Enums;
 
 namespace Demo.Mqtt
 {
     public class MqttContext : MqttBaseContext
     {
 
-        [Topic("factory_64/sensors/@/status")]
+        [Topic("factory_64/sensors/@/status", QoSLevel.AtMostOnce, true)]
         public TopicSet<DHT230222_Modules> DHT230222_Modules { get; set; }
 
         public MqttContext() : base("localhost", 1883) { }
