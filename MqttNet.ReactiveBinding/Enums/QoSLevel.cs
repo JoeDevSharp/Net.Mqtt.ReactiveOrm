e981da0dbedb395e8a3b9621c4caf79670e8f02e
José Miguel <j.ramos@jm-ramos.com>
2025-07-05 13:44:33 +0200

    Remove MqttNet.ReactiveBinding project and all source files
    Deleted the entire MqttNet.ReactiveBinding project, including all source code, interfaces, models, and project files. This removes the reactive MQTT object mapping functionality from the repository.

--- MqttNet.ReactiveBinding/Enums/QoSLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JoeDevSharp.MqttNet.ReactiveBinding.Enums
-{
-    public enum QoSLevel
-    {
-        AtMostOnce = 0x00,
-        AtLeastOnce = 0x01,
-        ExactlyOnce = 0x02
-    }
-}
